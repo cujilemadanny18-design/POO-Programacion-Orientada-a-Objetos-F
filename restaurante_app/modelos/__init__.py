704aea4fc22610258059032b45fb9a444ea013af
cujilemadanny18-design <cujilemadanny18@gmail.com>
2026-08-16 10:06:19 -1000

    Introduce Restaurante class with products and users
    Added Restaurante class to manage products and users.

--- restaurante_app/modelos/__init__.py
+++ restaurante_app/modelos/__init__.py
@@ -1,2 +1,11 @@
-from .producto import Producto
-from .cliente import Cliente
+from ast import List
+
+from restaurante_app.modelos.producto import Producto
+from restaurante_app.modelos.usuario import Usuario
+
+
+class Restaurante:
+
+    def __init__(self) -> None:
+        self.productos: List[Producto] = []
+        self.usuarios: List[Usuario] = []
